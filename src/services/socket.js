b04257d0857d47a68b4022af40fe27caf9d9a8af
charan1147 <charanc1147@gmail.com>
2025-07-09 18:08:46 +0530

    first commit1

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://project-quiz-backend-15.onrender.com', {
+const socket = io('https://project-quiz-backend-16.onrender.com', {
   withCredentials: true,
   transports: ['websocket'], 
 });
