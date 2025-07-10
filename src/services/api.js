b924f6b3275953c3a701c54319acc81d64462419
charan1147 <charanc1147@gmail.com>
2025-07-10 17:41:49 +0530

    first commit19

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://project-quiz-backend-19.onrender.com/api',
+  baseURL: 'https://project-quiz-backend-20.onrender.com/api',
   withCredentials: true, 
 });
 
--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://project-quiz-backend-19.onrender.com', {
+const socket = io('https://project-quiz-socket.onrender.com', {
   withCredentials: true,
   transports: ['websocket'], 
 });
